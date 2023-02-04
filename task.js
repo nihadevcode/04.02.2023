f5ac17a30999071e8072dee86c07069303d8d84f
Nihad Valiyev <nihadev@code.edu.az>
2023-02-04 23:15:32 +0400

    adding homework

---
+++ task.js
@@ -0,0 +1,56 @@
+//TASK1 1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+// let n = 22
+
+// if (n % 3 == 0 && n % 7 == 0) {
+//     console.log("n ededi 3-e ve 7-ye bolunur")
+// } else {
+//     console.log("n ededi 3-e ve 7-ye bolunmur")
+// }
+
+
+//TASK2 2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+// let n = 3;
+// let m = 4;
+
+// if (n % 2 == 0 && m % 2 == 0) {
+
+//     console.log(n + m)
+// }
+// else{
+//     console.log("error")
+// }
+
+//TASK3 3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin.
+
+// let n = 2;
+// let m = 8;
+// let count = 0;
+
+// for (let i = n; i < m; i++) {
+
+//     if (i % 2 == 1) {
+//         count++
+//     }
+//     console.log(count)
+// }
+
+//TASK4 4) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+let n = 2;
+let m = 8;
+let sum = 0;
+
+for (let i = n; i < m; i++) {
+
+    if (i % 2 == 1) {
+        sum++
+    }
+    console.log(sum)
+}
+
+
+
+
+
